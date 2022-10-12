d7c22568bf1bfe568d08ae262025839783d93ba6
donaldmurf <donaldmurf@gmail.com>
2022-10-11 23:30:04 -0400

    added color to helper

--- gitHelper.sh
+++ gitHelper.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
-echo "--------Sync git with GitHub--------"
-echo "git fetch --all -Pp"
-echo 
-echo "--------push update to branch--------"
-echo "git add ."
-echo "git commit -m “comment” "
-echo "git push" 
-echo 
-echo "--------remove and revert uncommited git changes--------"
-echo "git reset —hard"
-echo "git clean -fxd"
-echo 
-echo "--------displays help for git LFS--------"
-echo "git lfs migrate"
-echo 
-echo "--------roll back changes--------"
-echo "git log —oneline"
-echo "git reset <commit ID>" 
+clear
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+printf "${RED}--------Sync git with GitHub--------${NC}\n"
+printf "${GREEN}git fetch --all -Pp\n"
+printf "\n"
+printf "${RED}--------Push update to branch--------${NC}\n"
+printf "${GREEN}git add .\n${NC}"
+printf "${GREEN}git commit -m “comment” \n${NC}"
+printf "${GREEN}git push\n ${NC}" 
+printf "\n"
+printf "${RED}--------Remove and revert uncommited git changes--------${NC}\n"
+printf "${GREEN}git reset —hard\n${NC}"
+printf "${GREEN}git clean -fxd\n${NC}"
+printf "\n"
+printf "${RED}--------displays help for git LFS--------${NC}\n"
+printf "${GREEN}git lfs migrate\n${NC}"
+printf "\n"
+printf "${RED}--------roll back changes--------${NC}\n"
+printf "${GREEN}git log —oneline\n${NC}"
+printf "${GREEN}git reset <commit ID>\n${NC}" 
 
